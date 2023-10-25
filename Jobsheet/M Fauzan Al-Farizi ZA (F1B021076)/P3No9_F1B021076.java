b80adb387887799df7cdcd9957cca5b68c169941
dow00n <148226231+dow00n@users.noreply.github.com>
2023-10-25 23:07:33 +0800

    Add files via upload

---
+++ Jobsheet/M Fauzan Al-Farizi ZA (F1B021076)/P3No9_F1B021076.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class potensial {
+    double massa;
+    double tinggi;
+    double energiPotensial;
+    final double gravitasi = 9.81;
+ 
+    public potensial(double massa, double tinggi){
+        energiPotensial = massa*gravitasi*tinggi;
+    }
+
+    public double getEnergiPotensial(){
+        return energiPotensial;
+    }
+}
+
+public class P3No9_F1B021076 {
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Massa benda: ");
+        double massa = input.nextDouble();
+
+        System.out.println("Tinggi     : ");
+        double tinggi = input.nextDouble();
+
+        potensial energi = new potensial(massa, tinggi);
+        double hasilEnergiPotensial =energi.getEnergiPotensial();
+
+        System.out.println("Energi Potensial: " +hasilEnergiPotensial);
+    }
+}     
