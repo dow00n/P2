b79a88fc9382acd788504d3d0755ef7f0e78e4c6
dow00n <148226231+dow00n@users.noreply.github.com>
2023-10-25 23:06:16 +0800

    Add files via upload

---
+++ Jobsheet/M Fauzan Al-Farizi ZA (F1B021076)/P3No7_F1B021076.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class debit {
+    int volume;
+    int waktu;
+    int hitungDebit;
+
+    public debit(int volume, int waktu){
+        hitungDebit = volume/waktu;
+    }
+    
+    public int hitungDebit(){
+        return hitungDebit;
+    }
+}
+
+public class P3No7_F1B021076{
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Volume Air: ");
+        int volume = input.nextInt();
+
+        System.out.println("Waktu     : ");
+        int waktu = input.nextInt();
+        
+        debit nilai = new debit(volume, waktu);
+        System.out.println("Debit Air : " +nilai.hitungDebit);
+    }
+} 
