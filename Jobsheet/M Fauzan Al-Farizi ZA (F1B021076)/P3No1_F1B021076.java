5dd0fbb65abe099af80208954160b9dd22b91e91
dow00n <148226231+dow00n@users.noreply.github.com>
2023-10-25 23:02:08 +0800

    Add files via upload

---
+++ Jobsheet/M Fauzan Al-Farizi ZA (F1B021076)/P3No1_F1B021076.java
@@ -0,0 +1,16 @@
+class identitas{
+    String nama;
+
+    public identitas(){
+        nama = "Fauzan";
+    }
+}
+
+public class P3No1_F1B021076{
+    public static void main(String[] args){
+    identitas fzn = new identitas();
+
+    System.out.println("Nama Saya Adalah "+fzn.nama);
+    
+    }
+}
